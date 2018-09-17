dc14f18bacb11df7955f2148b93806c9df251b43
Mallory Feuer <mallory.feuer@gmail.com>
2018-09-17 17:19:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Write your code in this file!
 function scuberGreetingForFeet(someNumber) {
-  let greeting;
+  let greeting
   if (someNumber <= 400) {
-    greeting = "This one is on me!";
+    greeting = "This one is on me!"
   } else if (someNumber > 2000 && someNumber < 2501) {
-    greeting = "I will gladly take your thirty bucks.";
+    greeting = "I will gladly take your thirty bucks."
   } else {
-    greeting = "No can do.";
+    greeting = "No can do."
   }
   return greeting;
 } 
