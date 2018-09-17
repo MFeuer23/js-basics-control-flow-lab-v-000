5a83627ef16bebc8725675b32087120a21bcd6d6
Mallory Feuer <mallory.feuer@gmail.com>
2018-09-17 17:19:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // Write your code in this file!
 function scuberGreetingForFeet(someNumber) {
-  let greeting
+  let greeting;
   if (someNumber <= 400) {
-    greeting = "This one is on me!"
+    greeting = "This one is on me!";
   } else if (someNumber > 2000 && someNumber < 2501) {
-    greeting = "I will gladly take your thirty bucks."
+    greeting = "I will gladly take your thirty bucks.";
   } else {
-    greeting = "No can do."
+    greeting = "No can do.";
   }
   return greeting;
 } 
 
 function ternaryCheckCity(cityName) {
-  let response = cityName === "NYC" ? "Ok, sounds good." : "No go."
+  let response = cityName === "NYC" ? "Ok, sounds good." : "No go.";
   return response;
 }
